be6c25a3862b1c7e44e5e3045bd21916e591affa
Kimzooci <130586402+Kimzooci@users.noreply.github.com>
2024-04-07 04:33:52 +0900

    담당 components 개발
    담당한 컴포넌트 완.
    근데 깃 잘몰라서 vs에서 잘 안되네....
    일단 걍 깃허브에서 올림 ..

--- Re-forMe-Button.vue
+++ Re-forMe-Button.vue
@@ -1,13 +1,32 @@
 <template>
+    <button class="reforme-button">
+      리포유
+    </button>
+  </template>
   
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+  <script>
+  export default {
+    name: 'ReformeButton',
+    // 프롭스, 데이터, 메소드 등을 여기에 추가할 수 있습니다.
+  };
+  </script>
+  
+  <style scoped>
+  .reforme-button {
+    padding: 4px 21px;
+    background-color: #2E482D; /* 연한색하고 싶으면 #4A7648*/
+    color: white;
+    border: none;
+    font-size: 16px; /* 필요에 따라 크기를 조절하세요 */
+    cursor: pointer;
+    outline: none;
+    /* 폰트 가족과 다른 스타일을 원하는 대로 조정하세요 */
+  }
+  
+  /* 호버 효과 등 추가 스타일을 여기에 작성하세요 */
+  .reforme-button:hover {
+    /* 호버 스타일 예시 */
+    background-color: #1e3720;
+  }
+  </style>
+  
--- Re-forU-Button.vue
+++ Re-forU-Button.vue
@@ -1,13 +1,30 @@
 <template>
-  
+      <button class="reforU-button">
+      리포미
+    </button>
 </template>
 
 <script>
 export default {
-
+    name: 'ReforUbutton',
 }
 </script>
 
 <style>
-
+    .reforU-button {
+    padding: 4px 21px;
+    background-color: #4A7648; /* 연한색하고 싶으면 #4A7648* #2E482D */
+    color: white;
+    border: none;
+    font-size: 16px; /* 필요에 따라 크기를 조절하세요 */
+    cursor: pointer;
+    outline: none;
+    /* 폰트 가족과 다른 스타일을 원하는 대로 조정하세요 */
+  }
+  
+  /* 호버 효과 등 추가 스타일을 여기에 작성하세요 */
+  .reforU-button:hover {
+    /* 호버 스타일 예시 */
+    background-color: #1e3720;
+  }
 </style>
